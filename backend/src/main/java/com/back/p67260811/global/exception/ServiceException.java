6ab79d3caf36a92538ad0dc7ea98e858e2f66916
anton <anton061311@gmail.com>
2026-09-01 10:40:44 +0900

    feat: add authorId, authorName and postDto

---
+++ backend/src/main/java/com/back/p67260811/global/exception/ServiceException.java
@@ -0,0 +1,25 @@
+package com.back.p67260811.global.excpetion;
+
+import com.back.p67260811.global.dto.RsData;
+
+public class ServiceException extends RuntimeException {
+
+    private RsData rsData;
+
+    public ServiceException(String resultCode, String message) {
+        super(message);
+        this.rsData = new RsData(
+                resultCode,
+                message
+        );
+    }
+
+    public String getResultCode() {
+        return rsData.getResultCode();
+    }
+
+    public String getMessage() {
+        return rsData.getMessage();
+    }
+
+}
