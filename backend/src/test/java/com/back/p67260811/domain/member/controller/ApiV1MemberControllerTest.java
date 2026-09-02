6d0c49915c037f6f2603d1570e72bbcc93ed0e23
anton <anton061311@gmail.com>
2026-09-02 10:22:28 +0900

    feat(member): implement login functionality

--- backend/src/test/java/com/back/p67260811/domain/member/controller/ApiV1MemberControllerTest.java
+++ backend/src/test/java/com/back/p67260811/domain/member/controller/ApiV1MemberControllerTest.java
@@ -116,12 +116,12 @@ public class ApiV1MemberControllerTest {
                 .andExpect(handler().methodName("login"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.resultCode").value("200-1"))
-                .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(username)))
-                .andExpect(jsonPath("$.data.apiKey").exists());
-//                .andExpect(jsonPath("$.data.memberDto.id").value(member.getId()))
-//                .andExpect(jsonPath("$.data.memberDto.createDate").value(member.getCreateDate()))
-//                .andExpect(jsonPath("$.data.memberDto.modifyDate").value(member.getModifyDate()))
-//                .andExpect(jsonPath("$.data.memberDto.name").value(member.getName()));
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getNickname())))
+                .andExpect(jsonPath("$.data.apiKey").exists())
+                .andExpect(jsonPath("$.data.memberDto.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.memberDto.createDate").exists())
+                .andExpect(jsonPath("$.data.memberDto.modifyDate").exists())
+                .andExpect(jsonPath("$.data.memberDto.name").value(member.getNickname()));
 
     }
 }
