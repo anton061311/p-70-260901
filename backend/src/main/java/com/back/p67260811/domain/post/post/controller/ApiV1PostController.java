bd7508111f36864dae24898f549b0bad6dfc734b
anton <anton061311@gmail.com>
2026-09-03 09:30:09 +0900

    refactor(post): move authroization checks to Post entity

--- backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/back/p67260811/domain/post/post/controller/ApiV1PostController.java
@@ -96,11 +96,7 @@ public class ApiV1PostController {
 
         Member actor = rq.getActor();
         Post post = postService.findById(id).get();
-
-        if (!actor.equals(post.getAuthor())) {
-            throw new ServiceException("403-1", "수정 권한이 없습니다.");
-        }
-
+        post.checkActorModify(actor);
         postService.modify(post, reqBody.title, reqBody.content);
 
         return new RsData<>(
@@ -116,8 +112,8 @@ public class ApiV1PostController {
 
         Member actor = rq.getActor();
         Post post = postService.findById(id).get();
-        if (!actor.equals(post.getAuthor())) throw new ServiceException("403-1", "삭제 권한이 없습니다.");
 
+        post.checkActorDelete(actor);
         postService.delete(id);
 
         return new RsData<>(
